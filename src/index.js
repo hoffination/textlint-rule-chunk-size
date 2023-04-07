1b75704ca7e3f86843239fba18d2fabc78392c07
Ben Hofferber <hofferber.ben@gmail.com>
2023-04-07 18:52:36 -0400

    wip improvements

--- src/index.js
+++ src/index.js
@@ -10,17 +10,49 @@ export default function (context, options = {}) {
 
   return {
     [Syntax.Str](node) {
-      // "Str" node
-      const text = getSource(node); // Get text
-      console.log(chunkSize, delimiter, text);
-      const regex = new RegExp(
-        `(?<=${delimiter})[^(${delimiter})]{${chunkSize},}(${delimiter})`,
-        "g"
-      );
-      const matches = text.matchAll(regex);
-      for (const match of matches) {
-        const index = match.index ?? 0;
-        const matchRange = [index, index + match[0].length];
+      const text = getSource(node);
+      const regex = new RegExp(`${delimiter}`, "g");
+      let startIndex = 0;
+      let match;
+      let isFirstMatch = true;
+      let lastErrorStart = -1;
+
+      while ((match = regex.exec(text)) !== null) {
+        const endIndex = match.index;
+        const contentLength =
+          endIndex - startIndex - (isFirstMatch ? 0 : delimiter.length);
+
+        if (contentLength > chunkSize) {
+          if (lastErrorStart === -1) {
+            lastErrorStart = startIndex + (isFirstMatch ? 0 : delimiter.length);
+          }
+        } else {
+          if (lastErrorStart !== -1) {
+            const matchRange = [lastErrorStart, endIndex];
+            const ruleError = new RuleError("Found Error.", {
+              padding: locator.range(matchRange),
+            });
+            report(node, ruleError);
+            lastErrorStart = -1;
+          }
+        }
+        startIndex = endIndex;
+        isFirstMatch = false;
+      }
+
+      // Check the last segment
+      const remainingLength = text.length - startIndex - delimiter.length;
+      if (remainingLength > chunkSize) {
+        if (lastErrorStart === -1) {
+          lastErrorStart = startIndex + delimiter.length;
+        }
+        const matchRange = [lastErrorStart, text.length];
+        const ruleError = new RuleError("Found Error.", {
+          padding: locator.range(matchRange),
+        });
+        report(node, ruleError);
+      } else if (lastErrorStart !== -1) {
+        const matchRange = [lastErrorStart, startIndex];
         const ruleError = new RuleError("Found Error.", {
           padding: locator.range(matchRange),
         });
--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,32 @@
 import TextLintTester from "textlint-tester";
 import rule from "../src/index";
 
+const longString = `page: 1
+
+DUNGEON MASTERS GUIDE
+
+SPECIAL REFERENCE WORK
+
+DUNGEON MASTERS GUIDE
+
+A COMPILED VOLUME OF INFORMATION PRIMARILY USED BY ADVANCED DUNGEONS & DRAGONS'" GAME REFEREES, INCLUDING: COMBAT TABLES; MONSTER LISTS AND ENCOUNTERS; TREASURE AND MAGIC TABLES AND DESCRIPTIONS; RANDOM DUNGEON GENERATION; RANDOM WILDERNESS TERRAIN GENERATION; SUGGESTIONS ON GAMEMASTERING; AND MORE.
+
+By Gary Gygax © 1979 — T5R Gaines
+
+All rights reserved
+
+Illustrations by David C. Sutherland III D. A. Trampier Darlene Pekul Will McLean David S. La Force Erol Otus
+
+Cover by David C. Sutherland III
+
+FOREWORD
+
+Is Dungeon Mastering an art or a science? An interesting question!
+
+If you consider the pure creative aspect of starting from scratch, the "personal touch" of individual flair that goes into preparing and running a unique campaign, or the particular style of moderating a game adventure, then Dungeon Mastering may indeed be thought of as an art.
+
+If you consider the aspect of experimentation, the painstaking effort of preparation and attention to detail, and the continuing search for new ideas and approaches, then Dungeon Mastering is perhaps more like a science — not always exacting in a literal sense, but exacting in terms of what is required to do the job well.page: 1`;
+
 const tester = new TextLintTester();
 // ruleName, rule, { valid, invalid }
 tester.run("chunk size rule", rule, {
@@ -18,6 +44,7 @@ tester.run("chunk size rule", rule, {
   invalid: [
     // single match
     {
+      description: "match 10 error",
       text: "page: 123456789012\npage: 345",
       options: {
         chunkSize: 10,
@@ -26,11 +53,12 @@ tester.run("chunk size rule", rule, {
       errors: [
         {
           message: "Found Error.",
-          range: [6, 25],
+          range: [6, 19],
         },
       ],
     },
     {
+      description: "match 10 different delimiter error",
       text: "p: 123456789012\np: 345",
       options: {
         chunkSize: 10,
@@ -39,7 +67,20 @@ tester.run("chunk size rule", rule, {
       errors: [
         {
           message: "Found Error.",
-          range: [3, 19],
+          range: [3, 16],
+        },
+      ],
+    },
+    {
+      description: "match long string error",
+      text: longString,
+      options: {
+        chunkSize: 10,
+        delimiter: "page: ",
+      },
+      errors: [
+        {
+          message: "Found Error.",
         },
       ],
     },
